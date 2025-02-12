c764a8040df1edf9aaac8af658b12235280d72a7
rotavares <rotavares@proton.me>
2025-02-11 21:46:57 -0300

    exercício 13 incompleto

--- Cap3/ex13.c
+++ Cap3/ex13.c
@@ -1,4 +1,15 @@
 /* 13) Sejam a e b os catetos de um triângulo cuja hipotenusa h é
-obtida pela equação: h = v (a * a)+(b * b);
+obtida pela equação: h = raiz quadrada (a * a)+(b * b);
 */
+
+#include <stdio.h>
+
+int main() {
+    float a, b, h;
+
+    scanf("%f %f", &a, &b);
+    h = (a * a) + (b * b);
+    printf("%f", h);
+    return 0;
+}
 
