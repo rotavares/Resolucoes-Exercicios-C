8cddb3234b7b9d96925239397214f99db21f506f
rtavares <rotavares@proton.me>
2025-02-21 17:33:02 -0300

    iniciando exercício 9

---
+++ Cap4/ex9.c
@@ -0,0 +1,44 @@
+/* 9) Faça um programa que leia a altura e o peso de uma pessoa.
+De acordo com a tabela a seguir, verifique e mostre qual a
+classificação dessa pessoa.
+
+        Altura                                      Peso
+                                Até 60      Entre 60-90 (inclusive)     Acima de 90
+    Menor do que 1,20             A                   D                      G
+    1,20-1,70                     B                   E                      H
+    Maior do que 1,70             C                   F                      I
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    float altura, peso, classificacao;
+    printf("Digite a altura: ");
+    scanf("%f", &altura);
+    printf("Digite o peso: ");
+    scanf("%f", &peso);
+    if (altura < 1.20)
+    {
+        if (peso < 60)
+        {
+            printf("A\n");
+        }
+        if (peso > 60 && peso < 90)
+        {
+            printf("D\n");
+        }
+        if (peso > 90)
+        {
+            printf("G\n");
+        }
+        
+        if (altura > 1.20 && altura < 1.70)
+        {
+            
+        }
+        
+    }
+    return 0;
+}
