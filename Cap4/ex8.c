6c2d9dead7bce797ca5940745283dd054c0e81e4
rtavares <rotavares@proton.me>
2025-02-20 21:16:10 -0300

    exercício 8 finalizado

--- Cap4/ex8.c
+++ Cap4/ex8.c
@@ -7,47 +7,32 @@ classifique-o em uma das seguintes categorias:
         Juvenil  A          11-13
         Juvenil  B          14-17
         Sênior              maiores de 18 anos */
-
+        
 #include <stdio.h>
 
 int main() {
     int idade;
     printf("Digite a idade para ver a categoria: ");
     scanf("%d", &idade);
-    if (idade > 5 || idade < 7)
+    if (idade >= 5 && idade <= 7)
     {
         printf("Infantil A\n");
-    } else 
+    }
+    if (idade >= 8 &&  idade <= 10)
     {
-        if (idade > 8 || idade < 10)
-        {
-                printf("Infantil B\n");
-        } else 
-        {
-                if (idade > 11 || idade < 13)
-                {
-                        printf("Juvenil A\n");
-                } else 
-                {
-                        if (idade > 14 || idade < 17)
-                        {
-                                printf("Juvenil B\n");
-                        } else 
-                        {
-                                if (idade > 18)
-                                {
-                                        printf("Senior");
-                                } else 
-                                {
-                                        printf("idade minima é de 5 anos.\n");
-                                }
-                                
-                        }
-                        
-                }
-                
-        }
-        
+        printf("Infantil B\n");
+    }
+    if (idade >= 11 && idade <= 13)
+    {
+        printf("Juvenil A\n");
+    }
+    if (idade >= 14 && idade <= 17)
+    {
+        printf("Juvenil B\n");
+    }
+    if (idade > 18)
+    {
+        printf("Senior");
     }
     return 0;
 }
