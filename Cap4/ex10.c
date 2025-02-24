f3dad950e95c80c5a08d2302cae3640af7cf486e
rtavares <rotavares@proton.me>
2025-02-24 18:37:53 -0300

    exercício 9 completo

---
+++ Cap4/ex10.c
@@ -0,0 +1,34 @@
+/* 10) Faça um programa que leia três números inteiros positivos e
+efetue o cálculo de uma das seguintes médias de acordo com
+um valor numérico digitado pelo usuário e mostrado na tabela
+a seguir: 
+
+        Número digitado                      Média
+
+              1                            Geométrica  
+                                            x * y * z
+
+                                           Ponderada
+              2                         x + 2 * y + 3 * z
+                                        ----------------- 
+                                                6
+                                            
+                                            Harmônica
+              3                                 1
+                                            ---------
+                                            1   1   1
+                                            - + - + -
+                                            x   y   z
+                                         
+                                            Aritmética
+              4                             x + y + z
+                                            ---------
+                                                3
+    */
+
+# include <stdio.h>
+
+int main() {
+    unsigned int n1, n2, n3;
+    
+}
--- Cap4/ex9.c
+++ Cap4/ex9.c
@@ -33,12 +33,42 @@ int main()
         {
             printf("G\n");
         }
-        
+    }
+    else
+    {
         if (altura > 1.20 && altura < 1.70)
         {
-            
+            if (peso < 60)
+            {
+                printf("B\n");
+            }
+            if (peso > 60 && peso < 90)
+            {
+                printf("E\n");
+            }
+            if (peso > 90)
+            {
+                printf("H\n");
+            }
         }
-        
+        else
+        {
+            if (altura > 1.70)
+            {
+                if (peso < 60)
+                {
+                    printf("C\n");
+                }
+                if (peso > 60 && peso < 90)
+                {
+                    printf("F\n");
+                }
+                if (peso > 90)
+                {
+                    printf("I\n");
+                }
+            }
+                }
     }
     return 0;
 }
