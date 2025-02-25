b00bb13b474333d739b505eb46a98fcb73ba70d4
rtavares <rotavares@proton.me>
2025-02-25 17:40:06 -0300

    exercício 11 completo

--- Cap4/ex10.c
+++ Cap4/ex10.c
@@ -1,34 +1,73 @@
 /* 10) Faça um programa que leia três números inteiros positivos e
 efetue o cálculo de uma das seguintes médias de acordo com
 um valor numérico digitado pelo usuário e mostrado na tabela
-a seguir: 
+a seguir:
 
         Número digitado                      Média
 
-              1                            Geométrica  
+              1                            Geométrica
                                             x * y * z
 
                                            Ponderada
               2                         x + 2 * y + 3 * z
-                                        ----------------- 
+                                        -----------------
                                                 6
-                                            
+
                                             Harmônica
               3                                 1
                                             ---------
                                             1   1   1
                                             - + - + -
                                             x   y   z
-                                         
+
                                             Aritmética
               4                             x + y + z
                                             ---------
                                                 3
     */
 
-# include <stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-int main() {
-    unsigned int n1, n2, n3;
-    
+int main()
+{
+    float x, y, z, geo, pon, har, ari;
+    int valor;
+
+    printf("Digite tres numeros inteiros positivos: ");
+
+    scanf("%f %f %f", &x, &y, &z);
+
+    geo = pow(y * x * z, 1.0 / 3);
+
+    pon = (x + 2 * y + 3 * z) / 6;
+
+    har = 3 / (1 / x + 1 / y + 1 / z);
+
+    ari = (x + y + z) / 3;
+
+    printf("Escolha calculo da media de acordo os numeros:\n1 = Geometrica\n2 = Ponderada\n3 = Harmonica\n4 = Aritmetica\n");
+    scanf("%d", &valor);
+
+    if (valor == 1)
+    {
+        printf("media geometrica = %f\n", geo);
+    }
+    else if (valor == 2)
+    {
+        printf("media ponderada = %f\n", pon);
+    }
+    else if (valor == 3)
+    {
+        printf("media harmonica = %f\n", har);
+    }
+    else if (valor == 4)
+    {
+        printf("media aritmetica = %f\n", ari);
+    }
+    else
+    {
+        printf("\nerro: Digite um valor valor valido\n1 = Geometrica\n2 = Ponderada\n3 = Harmonica\n4 = Aritmetica\n");
+    }
+    return 0;
 }
---
+++ Cap4/ex11.c
@@ -0,0 +1,53 @@
+/* 11) Faça um programa que informe o mês de acordo com o
+número digitado pelo usuário. Exemplo: Entrada = 4.
+Saída = Abril. */
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um numero de 1 a 12 para exibir mes: ");
+    numero = getchar();
+    switch (numero)
+    {
+    case 1:
+        printf("Janeiro\n");
+        break;
+    case 2:
+        printf("Fevereiro\n");
+        break;
+    case 3:
+        printf("Março\n");
+        break;
+    case 4:
+        printf("Abril\n");
+        break;
+    case 5:
+        printf("Maio\n");
+        break;
+    case 6:
+        printf("Junho\n");
+        break;
+    case 7:
+        printf("Julho\n");
+        break;
+    case 8:
+        printf("Agosto\n");
+        break;
+    case 9:
+        printf("Setembro\n");
+        break;
+    case 10:
+        printf("Outubro\n");
+        break;
+    case 11:
+        printf("Novembro\n");
+        break;
+    case 12:
+        printf("Dezembro\n");
+        break; 
+    default: printf("erro: Digite um numero valido! 1 a 12\n");
+        break;
+    }
+}
