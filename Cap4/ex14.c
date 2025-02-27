1e27588d8b04a8f9ad4558efccab7e5a71a04727
rtavares <rotavares@proton.me>
2025-02-27 17:32:22 -0300

    exercício 14 completo

---
+++ Cap4/ex14.c
@@ -0,0 +1,31 @@
+/* 14) Faça um programa para verificar se determinado número
+inteiro lido é divisível por 3 ou 5, mas não simultaneamente
+pelos dois. */
+
+#include <stdio.h>
+
+int main()
+{
+    int num, contagem = 0;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    if (num % 5 == 0)
+    {
+        contagem++;
+    }
+    if (num % 3 ==0)
+    {
+        contagem++;
+    }
+    if (contagem == 0)
+    {
+        printf("Este numero nao e divisivel por 3 ou 5.");
+    } else if (contagem == 1)
+    {
+        printf("Este numero e divisivel apenas por um dos numeros");   
+    } else 
+    {
+        printf("Este numero e divisivel pelo dois numeros");
+    }
+    return 0;    
+}
