840d793e9674b45b765558acd3e387046f3242c9
rtavares <rotavares@proton.me>
2025-03-05 17:22:44 -0300

    exercício 15 completo

--- Cap4/ex15.c
+++ Cap4/ex15.c
@@ -21,12 +21,32 @@ raiz e a mensagem “Raiz única”.
 Imprima as raízes. */
 
 #include <stdio.h>
-#include <math.c>
-int main () , {
-    float a, b, c, delta, r1, r2;
+#include <math.h>
+
+int main () {
+    float a, b, c, delta, x1, x2;
     printf("Digite os coeficientes: ");
-    scanf("%f %f %f", &a, &b, &c);
+    scanf("%f%f%f", &a, &b, &c);
+    if (a == 0)
+    {
+        printf("Nao e equacao de segundo grau\n");
+        return 0;
+    }
     delta = pow(b, 2) - 4 * a * c;
-    
+    if (delta < 0)
+    {
+        printf("Nao existe raiz\n");
+        return 0;
+    }
+    if (delta == 0)
+    {
+        x1 = - b / (2 * a);
+        printf("%f, Raiz unica\n", x1);
+        return 0;
+    }
+    x1 = (- b + sqrt(delta)) / (2 * a);
+    x2 = (- b - sqrt(delta)) / (2 * a);
+    printf("x1 = %f\n", x1);
+    printf("x2 = %f\n", x2);
     return 0;
 }
