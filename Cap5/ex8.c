f97ef7ecaec7ae52e1d0eb23ca3138a3ecf9469e
rtavares <rotavares@proton.me>
2025-03-12 17:59:20 -0300

    exercício 8 completo

---
+++ Cap5/ex8.c
@@ -0,0 +1,15 @@
+/* 8) Faça um programa que leia 10 inteiros e imprima sua média. */
+
+#include <stdio.h>
+
+int main() {
+    int n, soma = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("Digite %d# valor = ", i);
+        scanf("%d", &n);
+        soma += n;
+    }
+    printf("media = %.2f\n", (float) soma / 10);
+    return 0;
+}
