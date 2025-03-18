38125b57bc06a1a809b8b902cd9b6f8268a44beb
rtavares <rotavares@proton.me>
2025-03-18 16:26:35 -0300

    exercício 14 completo

--- Cap5/ex14.c
+++ Cap5/ex14.c
@@ -8,7 +8,18 @@ sequência são: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34. */
 #include <stdio.h>
 
 int main() {
-    int num, fibonacci;
-    scanf("%d", &num);
-    
+    int var1 = 0;
+    int var2 = 1;
+    int var3 = 1;
+    int enesimo;
+    printf("digite o valor = ");
+    scanf("%d", &enesimo);
+    for (int i = 0; i < enesimo; i++)
+    {
+        printf("%d ", var3);
+        var3 = var2 + var1;
+        var1 = var2;
+        var2 = var3;
+    }
+    return 0;
 }
