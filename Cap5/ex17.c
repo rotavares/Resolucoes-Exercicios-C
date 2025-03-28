6099d4d5c2010ecd84d3427dd6bdfa44df54787a
rtavares <rotavares@proton.me>
2025-03-28 17:52:20 -0300

    exercício 17 finalizado

--- Cap5/ex17.c
+++ Cap5/ex17.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    
+    int linhas, contador = 1;
+    printf("Digite o numero de linhas: ");
+    scanf("%d", &linhas);
+    for (int i = 1; i <= linhas; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", contador);
+            contador++;
+        }
+        printf("\n");
+    }
+    return 0;
 }
