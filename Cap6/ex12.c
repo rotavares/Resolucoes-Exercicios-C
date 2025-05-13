270de8ede554265e77b83e9e21758e5f2900b66a
rtavares <rotavares@proton.me>
2025-05-13 17:49:18 -0300

    iniciando exercício 13

--- Cap6/ex12.c
+++ Cap6/ex12.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    int contagem = 0, vetor[10];
- 
+int main()
+{
+    int vetor[10];
+
     for (int i = 0; i < 10; i++)
     {
         scanf("%d", &vetor[i]);
     }
-    
 
     for (int i = 0; i < 10; i++)
     {
-        for (int j = 0; j < 10; i++)
+        for (int j = i + 1; j < 10; j++)
         {
-            if (vetor[i] == vetor[j]);
-            contagem++;
+            if (vetor[i] == vetor[j])
+            {
+                printf("%d repete\n", vetor[i]);
+            }
         }
-        if (contagem > 2)
-        {
-            printf("%d ", vetor[i]);
-        }
-        printf("\n");
-        contagem = 0;
     }
-    
     return 0;
 }
