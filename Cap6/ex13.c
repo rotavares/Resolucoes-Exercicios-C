aa2bdd13f2b037f8a9a24f8aca6e5f82058e40c1
rtavares <rotavares@proton.me>
2025-05-14 17:25:50 -0300

    iniciando exercício 14

--- Cap6/ex13.c
+++ Cap6/ex13.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    
+int main()
+{
+    int i = 0, vetor[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &vetor[i]);
+        for (int j = 0; j < i; j++)
+        {
+            while (vetor[i] == vetor[j])
+            {
+                printf("valor repetido, digite novamente.\n");
+                scanf("%d", &vetor[i]);
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
 }
