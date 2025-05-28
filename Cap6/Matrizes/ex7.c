3a64c65a255c1aa10930c36980c564a9e6182ccd
rotavares <rotavares@proton.me>
2025-05-28 17:04:51 -0300

    exercício 7 finalizado

---
+++ Cap6/Matrizes/ex7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int soma = 0, matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i == j)
+            {
+                soma += matriz[i][j];
+            }
+        }
+    }
+    printf("Soma = %d\n", soma);
+    return 0;
+}
