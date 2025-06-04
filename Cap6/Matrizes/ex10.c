a211ace8697edd9005d44216e46e74e7f0c0a9d3
rotavares <rotavares@proton.me>
2025-06-04 17:26:07 -0300

    iniciando exercício 11

--- Cap6/Matrizes/ex10.c
+++ Cap6/Matrizes/ex10.c
@@ -2,22 +2,28 @@
 
 int main() 
 {
-    float matriz[3][10];
+    float vetor[3], matriz[3][10];
+    float piorNota;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 10; j++)
         {
+            printf("%d# nota aluno: ", j+1);
             scanf("%f", &matriz[i][j]);
         }
     }
 
     for (int i = 0; i < 3; i++)
     {
+        piorNota = matriz[i][0];
         for (int j = 0; j < 10; j++)
         {
-            printf("%.2f ", matriz[i][j]);
+            if (piorNota > matriz[i][j])
+            {
+                piorNota = matriz[i][j];
+            }       
         }
-        printf("\n");
+        printf("Pior nota prova %d: %.2f\n", i+1, piorNota);
     }
     return 0;
 }
---
+++ Cap6/Matrizes/ex11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int soma = 0;
+    int matriz[5][5] = {{45,78,41,52,35},{51,59,24,26,27},{17,24,19,26,81},{34,39,83,67,55},{52,41,53,61,43}};
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i != j && i < 3)
+            {
+                soma += matriz[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("soma elementos acima da diagonal principal = %d\n", soma);
+    return 0;
+}
