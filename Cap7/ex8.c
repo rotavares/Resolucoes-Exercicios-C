0b6d8dc8d8939f6d12480cd3a00973556359784a
Rodrigo Tavares <rotavares@proton.me>
2025-08-13 14:04:23 -0300

    exercício 8 finalizado

--- Cap7/ex8.c
+++ Cap7/ex8.c
@@ -1,72 +1,44 @@
-/*Construa um programa que leia duas strings do teclado. 
-Imprima uma mensagem informando se a segunda string lida está contida dentro da primeira.*/
-
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIZE 25
+#define MAX_SIZE 128
 
-int main() {
-    char string1[MAX_SIZE];
-    char string2[MAX_SIZE];
+int main() 
+{
+  char string[MAX_SIZE];
+  char check[MAX_SIZE];
+  char aux[MAX_SIZE];
 
-    printf("digite a primeira frase: ");
-    fgets(string1, MAX_SIZE, stdin);
+  int i, j;
 
-    int i = 0;
-    while (string1[i] != '\0' && string1[i] != '\n') {
-        i++;
-    }
-    if (string1[i] == '\n') {
-        string1[i] = '\0';
-    }
+  printf("Digite a frase: ");
+  fgets(string, MAX_SIZE, stdin);
+  string[strlen(string) - 1] = '\0';
 
-    printf("digite a segunda frase: ");
-    fgets(string2, MAX_SIZE, stdin);
+  printf("Digite a palavra: ");
+  fgets(check, MAX_SIZE, stdin);
+  check[strlen(check) - 1] = '\0';
 
-    i = 0;
-    while (string2[i] != '\0' && string2[i] != '\n') {
-        i++;
-    }
-    if (string2[i] == '\n') {
-        string2[i] = '\0';
-    }
+  for (i = 0, j = 0; string[i] != '\0'; i++) {
 
-    int len1 = 0;
-    while (string1[len1] != '\0') {
-        len1++;
+    while (string[i] != ' ' && string[i] != '\0') {
+
+      aux[j] = string[i];
+      i++;
+      j++;
     }
 
-    int len2 = 0;
-    while (string2[len2] != '\0') {
-        len2++;
-    }
+    aux[j] = '\0';
 
-    int encontrada = 0;
+    if (strcmp(aux, check) == 0) {
+      printf("'%s' está contida em '%s'.\n", check, string);
+      return 0;
+    }
+    else {
+      j = 0;
+    }
+  }
 
-    if (len2 > len1) {
-        encontrada = 0;
-    } else {
-        for (int i = 0; i <= len1 - len2; i++) {
-            int j = 0; 
-            int match = 1;
-			while (j < len2) {
-                if (string1[i + j] != string2[j]) {
-                    match = 0;
-					break;
-                }
-                j++;
-            }
-			if (match == 1) {
-                encontrada = 1;
-                break;
-            }
-        }
-    }
-    if (encontrada) {
-        printf("'%s' esta contida em '%s'.\n", string2, string1);
-    } else {
-        printf("'%s' não esta contida em '%s'.\n", string2, string1);
-    }
-    return 0;
+  printf("'%s' não está contida em '%s'.\n", check, string);
+  return 0;
 }
