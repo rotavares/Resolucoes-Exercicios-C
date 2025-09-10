270760260c659b83ab216021a6b8b39739bee653
Rodrigo <rotavares@proton.me>
2025-09-09 21:35:26 -0300

    exercício 8 finalizado

--- Cap8/ex8.c
+++ Cap8/ex8.c
@@ -11,23 +11,55 @@ int main() {
 
 	for (i = 0; i < 6; i++) {
 		setbuf(stdin, NULL);
-		printf("%d#  Insira o nome: ", i + 1);
+		printf("%d# Nome: ", i + 1);
 		fgets(p[i].nome, 50, stdin);
-		printf("%d# Dia nascimento: ", i + 1);
-		scanf("%d", &p[i].dia);
-		printf("%d# Mês nascimento: ", i + 1);
-		scanf("%d", &p[i].mes);
-		printf("%d# Ano nascimento: ", i + 1);
-		scanf("%d", &p[i].ano);
-	}
-	
-	for (i = 0; i < 6; i++) {
-		if (i == 0) {
-			
-		}
+		printf("%d# Data nascimento: ", i + 1);
+    scanf("%d%d%d", &p[i].dia, &p[i].mes, &p[i].ano);
 	}
 
-	printf("Mais velho: %sMais novo: %s", p[pmenor].nome, p[pmaior].nome);
+  for (i = 0; i < 6; i++) {
+
+    if (i == 0) {
+      maiorIdade = i;
+      menorIdade = i;
+    }
+
+    if (p[menorIdade].ano < p[i].ano) {
+     menorIdade = i;
+    }
+    else if (p[menorIdade].ano == p[i].ano) {
+
+      if (p[menorIdade].mes < p[i].mes) {
+        menorIdade = i;
+      }
+      else if (p[menorIdade].mes == p[i].mes) {
+        menorIdade = i;
+
+        if (p[menorIdade].dia < p[i].dia) {
+          menorIdade= i;
+        }
+      }
+    }
+
+    if (p[maiorIdade].ano > p[i].ano) {
+      maiorIdade = i;
+    }
+    else if (p[maiorIdade].ano == p[i].ano) {
+
+      if (p[maiorIdade].mes > p[i].mes) {
+        maiorIdade = i;
+      }
+      else if (p[maiorIdade].mes == p[i].mes) {
+        maiorIdade = i;
+
+        if (p[maiorIdade].dia > p[i].dia) {
+          maiorIdade = i;
+        }
+      }
+    }
+  }
+
+  printf("Mais velho: %sMais novo: %s", p[maiorIdade].nome, p[menorIdade].nome);
 
 	return 0;
 }
