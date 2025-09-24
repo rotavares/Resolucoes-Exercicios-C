f9513548a6349b7292fe4d803521f52850b843fb
Rodrigo <rotavares@proton.me>
2025-09-24 14:27:19 -0300

    exercício 10 finalizado

--- Cap8/ex10.c
+++ Cap8/ex10.c
@@ -1,41 +1,75 @@
-#include <stdio.h>
+/* Usando a estrutura “atleta” do exercício anterior, escreva um programa que leia os dados de cinco atletas e os exiba por ordem de idad , do mais velho para o mais novo. */
 
-struct Atleta {
-  char nome[50];
-  char esporte[20];
-  int idade;
-  float altura;
+#include <stdio.h>
+#include <string.h>
+
+#define QTD_ATLETAS 5
+
+struct atleta {
+    char nome[32];
+    char esporte[16];
+    unsigned short idade;
+    double altura;
 };
 
 int main() {
-  
-  struct Atleta a[5];
-  int i, j;
+    struct atleta a[QTD_ATLETAS] = {0};
 
-  for (i = 0; i < 5; i++) {
-    setbuf(stdin, NULL);
-    printf("%d# Nome: ", i+1);
-    fgets(a[i].nome, 50, stdin);
-    printf("Esporte: ");
-    fgets(a[i].esporte, 20, stdin);
-    printf("Idade: ");
-    scanf("%d", &a[i].idade);
-    printf("Altura: ");
-    scanf("%f", &a[i].altura);
-  }
+    printf("Digite os dados dos atletas\n");
 
-  for (i = 0; i < 5; i++) {
-    for (j = i; j < 5; j++) {
-      if (a[j].idade > a[i].idade) {
-        struct Atleta aux = a[i];
-        a[i] = a[j];
-        a[j] = aux;
-      }
+    for (int i = 0; i < QTD_ATLETAS; i++) {
+        printf("\t--- #%d Cadastro ---\n", i+1);
+
+        printf("\tNome: ");
+        fgets(a[i].nome, 32, stdin);
+
+        int len = strlen(a[i].nome);
+        if (a[i].nome[len-1] == '\n') {
+            a[i].nome[len-1] = '\0';
+        } else {
+            setbuf(stdin, NULL);
+        }
+
+        printf("\tEsporte : ");
+        fgets(a[i].esporte, 16, stdin);
+
+        len = strlen(a[i].esporte);
+        if (a[i].esporte[len-1] == '\n') {
+            a[i].esporte[len-1] = '\0';
+        } else {
+            setbuf(stdin, NULL);
+        }
+
+        printf("\tIdade: ");
+        scanf("%hu", &a[i].idade);
+        setbuf(stdin, NULL);
+
+        printf("\tAltura: ");
+        scanf("%lf", &a[i].altura);
+        setbuf(stdin, NULL);
     }
-  }
 
-  for (i = 0; i < 5; i++) {
-    printf("\nNome: %sEsporte: %sIdade: %d\nAltura: %.2f\n", a[i].nome, a[i].esporte, a[i].idade, a[i].altura);
-  }
-  return 0;
+    for (int i = 0; i < QTD_ATLETAS; i++) {
+        for( int j = i; j < QTD_ATLETAS; j++) {
+            if (a[j].idade > a[i].idade) {
+                struct atleta temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("Atletas por ordem de idade do mais velho ao mais novo: ");
+
+    for (int i = 0; i < QTD_ATLETAS; i++) {
+        if (i != (QTD_ATLETAS - 1)) {
+            printf("%s, ", a[i].nome);
+        } else {
+            printf("%s.\n", a[i].nome);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
 }
